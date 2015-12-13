f30a72664294580690a51384dca07ae73dca6b39
Noah Morton <noahmorton98@gmail.com>
2015-12-12 18:47:27 -0600

    Optimisations to Proximity door, thanks Molinko

--- OpenComputers/HeadlessPrograms/ProximityDoor.lua
+++ OpenComputers/HeadlessPrograms/ProximityDoor.lua
@@ -20,19 +20,24 @@ local acceptableEntities = {...} --args
 if type(tonumber(acceptableEntities[1])) ~= "number" then error("Please provide time for door to stay open as the first arg.") end
 local stayOpen = tonumber(acceptableEntities[1])
 if #acceptableEntities <= 1 then error("Someone needs to have access!") end
+table.remove(acceptableEntities,1) --remove time from list of entities
 
-
+--add list of players to lookup list
+players = {}
+for _,name in pairs(acceptableEntities) do
+  if type(tostring(name)) ~= "string" then error("Please provide entity names only.") end --sanity check
+  players[tostring(name)] = true
+end
 
 while true do
+  redstone.setOutput(side,0)
   local _,_,x,y,z,entity = event.pull("motion")
   if math.abs(x)<= tonumber(range) and math.abs(y)<= tonumber(range) and math.abs(z)<= tonumber(range) then --if recieved motion was within range
-    for p = 2,#acceptableEntities do
-      if tostring(acceptableEntities[p]) == tostring(entity) then
-        redstone.setOutput(side,15)
-        os.sleep(stayOpen)
-        redstone.setOutput(side,0)
-        break --no need to continue testing if one is valid
-      end
+    if players[tostring(entity)] then --if entity is on whitelist
+      redstone.setOutput(side,15)
+      os.sleep(stayOpen)
+      redstone.setOutput(side,0)
     end
   end
 end
+--eof
