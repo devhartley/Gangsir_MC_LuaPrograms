36d07d3fea60c27a3f3da9d52b91915d57fe7ea6
Noah Morton <noahmorton98@gmail.com>
2015-12-14 17:24:22 -0600

    Updated proximity door to v1.2, added blacklist mode

--- OpenComputers/HeadlessPrograms/ProximityDoor.lua
+++ OpenComputers/HeadlessPrograms/ProximityDoor.lua
@@ -8,6 +8,7 @@ If door is too large for one detector, multiple can be used
 Switches:
 -a : Accept all mode, opens for any motion
 -i : Inverted mode, turns off redstone on detect, good for piston doors
+-b : Blacklist mode, prevents entity on blacklist from opening the door, accepts all others
 --]]
 
 component = require("component")
@@ -26,19 +27,30 @@ local range = 3 --distance away from motion sensor to check
 local side = sides.top
 
 
-players = {} --holds whitelist of players/entities
+allowed = {} --holds whitelist of players/entities
+banned = {}
 local acceptableEntities, options = require("shell").parse(...)
 if type(tonumber(acceptableEntities[1])) ~= "number" then error("Please provide time for door to stay open as the first arg.") end
 local stayOpen = tonumber(acceptableEntities[1])
 
+if options.a and options.b then error("You can't accept all and have a blacklist. Remove one or the other.") end
+
 if not options.a then --if not accept all mode
-  if #acceptableEntities <= 1 then error("Someone needs to have access!") end
+  if #acceptableEntities <= 1 then error("Someone needs to be on the list!") end
   table.remove(acceptableEntities,1) --remove time from list of entities
-  print("Listing names of valid entities: ")
+  print("Listing names of provided entities: ")
   --add list of players to lookup list
-  for _,name in pairs(acceptableEntities) do
-    players[name] = true --set value in table to true, for quick lookup later
-    print(name) --print the name of the registered entity
+  if not options.b then --if using list as whitelist
+    print("Running in blacklist mode, blacklisting all provided entity names...")
+    for _,name in pairs(acceptableEntities) do
+      allowed[name] = true --set value in table to true, for quick lookup later
+      print("Allowed: "..name) --print the name of the registered entity
+    end
+  else --whitelist acts as blacklist
+    for _,name in pairs(acceptableEntities) do
+      banned[name] = true --set value in table to true, for quick lookup later
+      print("Banned: "..name) --print the name of the registered entity
+    end
   end
 else
   print("Running in accept all mode, door will open for any motion.")
@@ -66,10 +78,12 @@ print("Program setup complete. You may now remove all graphics components. Door 
 while true do
   local _,_,x,y,z,entity = event.pull("motion")
   if math.abs(x)<= tonumber(range) and math.abs(y)<= tonumber(range) and math.abs(z)<= tonumber(range) then --if recieved motion was within range
-    if players[entity] or options.a then --if entity is on whitelist, or accept all mode is on
+    if options.a or allowed[entity] or banned[entity]==nil then --if entity is on whitelist, or accept all mode is on, or not banned
       toggleRedstone()
       os.sleep(stayOpen) --manipulate door
       toggleRedstone()
+    else
+      print("Banned entity "..entity.." tried opening the door!") --print for security logs
     end
   end
 end
