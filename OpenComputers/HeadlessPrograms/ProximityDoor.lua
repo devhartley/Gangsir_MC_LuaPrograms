092045a1415197e4f331392d577cc3e13d0709ae
Noah Morton <noahmorton98@gmail.com>
2015-12-14 16:47:39 -0600

    Updated proximity door to v1.1, added accept all mode and inverted mode

--- OpenComputers/HeadlessPrograms/ProximityDoor.lua
+++ OpenComputers/HeadlessPrograms/ProximityDoor.lua
@@ -5,6 +5,9 @@ ex: door 5 Gangsir dan200 Sangar
 requires a motion detector attached to the computer, and a redstone card.
 Headless, screen not needed after run
 If door is too large for one detector, multiple can be used
+Switches:
+-a : Accept all mode, opens for any motion
+-i : Inverted mode, turns off redstone on detect, good for piston doors
 --]]
 
 component = require("component")
@@ -23,30 +26,50 @@ local range = 3 --distance away from motion sensor to check
 local side = sides.top
 
 
-local acceptableEntities = {...} --args
+players = {} --holds whitelist of players/entities
+local acceptableEntities, options = require("shell").parse(...)
 if type(tonumber(acceptableEntities[1])) ~= "number" then error("Please provide time for door to stay open as the first arg.") end
 local stayOpen = tonumber(acceptableEntities[1])
-if #acceptableEntities <= 1 then error("Someone needs to have access!") end
-table.remove(acceptableEntities,1) --remove time from list of entities
 
-print("Listing names of valid entities: ")
---add list of players to lookup list
-players = {}
-for _,name in pairs(acceptableEntities) do
-  if type(tostring(name)) ~= "string" then error("Please provide entity names only.") end --sanity check
-  players[tostring(name)] = true --set value in table to true, for quick lookup later
-  print(name) --print the name of the registered entity
+if not options.a then --if not accept all mode
+  if #acceptableEntities <= 1 then error("Someone needs to have access!") end
+  table.remove(acceptableEntities,1) --remove time from list of entities
+  print("Listing names of valid entities: ")
+  --add list of players to lookup list
+  for _,name in pairs(acceptableEntities) do
+    players[name] = true --set value in table to true, for quick lookup later
+    print(name) --print the name of the registered entity
+  end
+else
+  print("Running in accept all mode, door will open for any motion.")
 end
 
-print("Program setup complete. You may now remove all graphics components. Door will stay open for "..tostring(stayOpen).." seconds on detection.")
+--toggles current output of redstone on and off, returns new state as bool
+function toggleRedstone()
+  if redstone.getOutput(side) >=1 then
+    redstone.setOutput(side,0)
+    return false
+  else
+    redstone.setOutput(side,15)
+    return true
+  end
+end
+
+if options.i then --if running inverted mode, keep redstone on by default
+  redstone.setOutput(side,15)
+  print("Running in inverted mode, redstone turns off on detect.")
+else
+  redstone.setOutput(side,0)
+end
+
+print("Program setup complete. You may now remove all graphics components. Door will toggle state for "..tostring(stayOpen).." seconds on detection.")
 while true do
-  redstone.setOutput(side,0) --reset redstone
   local _,_,x,y,z,entity = event.pull("motion")
   if math.abs(x)<= tonumber(range) and math.abs(y)<= tonumber(range) and math.abs(z)<= tonumber(range) then --if recieved motion was within range
-    if players[entity] then --if entity is on whitelist
-      redstone.setOutput(side,15)
-      os.sleep(stayOpen)
-      redstone.setOutput(side,0)
+    if players[entity] or options.a then --if entity is on whitelist, or accept all mode is on
+      toggleRedstone()
+      os.sleep(stayOpen) --manipulate door
+      toggleRedstone()
     end
   end
 end
